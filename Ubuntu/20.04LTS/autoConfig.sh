76c4361b20b54d00bf40d9d9db56d90e3cb23ce8
chenyj14 <chenyj14@xiaopeng.com>
2022-12-09 09:46:14 +0800

    update work_path

--- Ubuntu/20.04LTS/autoConfig.sh
+++ Ubuntu/20.04LTS/autoConfig.sh
@@ -7,10 +7,10 @@
 # Update the apt package index
 yes y | sudo apt-get update
 
-HOME_PATH=~/home/ubuntu
+WORK_PATH=/home/ubuntu
 
 # Install JDK8
-cd $HOME_PATH/QCSR/Ubuntu/20.04LTS
+cd $WORK_PATH/QCSR/Ubuntu/20.04LTS
 
 chmod 777 ./scripts/*
 
